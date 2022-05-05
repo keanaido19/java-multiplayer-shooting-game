eb587bfadcc16942f510a4478e317f1cb53c229d
Amanda Gumede <amagumed021@student.wethinkcode.co.za>
2022-05-05 13:32:34 +0000

    Update Client.java

--- src/main/java/RobotWorld/client/Client.java
+++ src/main/java/RobotWorld/client/Client.java
@@ -11,17 +11,36 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    static Scanner scanner;
+ static Scanner scanner;
     public static void main(String[] args) throws IOException {
+        scanner = new Scanner(System.in);
+        Robot robot;
+        String name = getInput("What do you want to name your robot?");
+        robot = new Robot(name);
+        System.out.println(robot.toString());
 
-        try (
+        Command command=null;
+        boolean shouldContinue = true;
+
+        try(
                 Socket socket = new Socket("localhost", 5000);
                 PrintStream out = new PrintStream(socket.getOutputStream());
                 BufferedReader in = new BufferedReader(new InputStreamReader(
-                        socket.getInputStream()))
-        ){
+                        socket.getInputStream()));
+        ) {
+            do {
+                String instruction = getInput(robot.getName() + "> What must I do next?").strip().toLowerCase();
 
-            out.println("hello");
+                try {
+                    command = Command.create(instruction);
+                    shouldContinue = robot.handleCommand(command);
+
+                } catch (IllegalArgumentException e) {
+                    robot.setStatus("Sorry, I did not understand '" + instruction + "'.");
+                }
+            } while (shouldContinue);
+
+            out.println(command);
             out.flush();
             String messageFromServer = in.readLine();
             System.out.println("Response: "+messageFromServer);
@@ -29,7 +48,21 @@ public class Client {
             e.printStackTrace();
         }
     }
+
+    private static String getInput(String prompt) {
+        System.out.println(prompt);
+        String input = scanner.nextLine();
+
+
+        while (input.isBlank()) {
+            System.out.println(prompt);
+            input = scanner.nextLine();
+        }
+        return input;
+    }
 }
+
+
 
 
 
