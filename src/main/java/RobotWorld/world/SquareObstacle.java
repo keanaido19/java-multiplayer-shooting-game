7ae36ec4b18c9845593888c737bd10c2645a02fc
Lundiphile Matomela <lmatomel021@student.wethinkcode.co.za>
2022-05-06 07:21:55 +0000

    Updated SquareObstacle.java

--- src/main/java/RobotWorld/world/SquareObstacle.java
+++ src/main/java/RobotWorld/world/SquareObstacle.java
@@ -5,11 +5,13 @@ import RobotWorld.robot.Position;
 public class SquareObstacle implements Obstacle{
     private int x;
     private int y;
+    public final int size;
 
-    public SquareObstacle(int x, int y){
+    public SquareObstacle(int x, int y)
+    {
         this.x = x;
+        this.size = 5;
         this.y = y;
-
     }
 
     @Override
@@ -24,31 +26,28 @@ public class SquareObstacle implements Obstacle{
 
     @Override
     public int getSize() {
-        return 5;
+        return size;
     }
 
     @Override
-    public boolean blocksPosition(Position position) {
-        if ( this.x<= position.getX()&&position.getX() < this.x+5  &&
-                this.y <= position.getY()&&position.getY() < this.y+5)
-        {
-            return true;
-        }
-        return  false;
+    public boolean blocksPosition(Position position)
+    {
+        int xCor = position.getX(), yCor = position.getY();
+
+        return this.x <= xCor&&xCor < this.x + size
+                && this.y <= yCor&&yCor < this.y + size;
     }
 
     @Override
-    public boolean blocksPath(Position a, Position b) {
-            if(a.getY()<= this.y&&this.y< b.getY() &&
-                this.x <= a.getX()&&a.getX() < this.x +5){
-                 return true;
-            }
-            else
-                if(a.getX() <= this.x&& this.x< b.getX() &&
-                    this.y <= a.getY()&&a.getY()< this.y +5 ){
-                    return true;
-        }
+    public boolean blocksPath(Position a, Position b)
+    {
+        int maxX = Math.max(a.getX(),b.getX()), minX = Math.min(a.getX(),b.getX());
+        int maxY = Math.max(a.getY(),b.getY()), minY = Math.min(a.getY(),b.getY());
 
-        return false;
+        if (a.getX() == b.getX())
+            return minY <= this.y&&this.y <= maxY && this.x <= a.getX()&&a.getX() < this.x + size;
+        else
+            return minX <= this.x&&this.x <= maxX && this.y <= a.getY()&&a.getY() < this.y + size;
+
     }
 }
